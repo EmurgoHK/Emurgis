22a74b17a9bf3020b2ffaecc22d363366c060a54
Ash Smith <missenlinx@gmail.com>
2018-06-04 20:36:57 +0800

    problem: no count of comments on index
    solution: add count of comments on index page #134

--- imports/ui/components/documents/index/documents-index-item/documents-index-item.js
+++ imports/ui/components/documents/index/documents-index-item/documents-index-item.js
@@ -3,12 +3,17 @@ import SimpleSchema from "simpl-schema"
 
 import { notify } from "/imports/modules/notifier"
 import { claimProblem, unclaimProblem, deleteProblem } from "/imports/api/documents/both/problemMethods.js"
+import { Comments } from "/imports/api/documents/both/commentsCollection.js"
 
 import "./documents-index-item.html"
 import "/imports/ui/components/documents/shared/problem-helpers.js"
 
 Template.documentsIndexItem.onCreated(function() {
-  this.getDocumentId = () => Template.instance().data.document._id
+  this.getDocumentId = () => FlowRouter.getParam("documentId")
+
+  this.autorun(() => {
+    this.subscribe("comments", this.getDocumentId())
+  })
 })
 
 Template.documentsIndexItem.onRendered(function() {})
@@ -16,6 +21,10 @@ Template.documentsIndexItem.onRendered(function() {})
 Template.documentsIndexItem.onDestroyed(function() {})
 
 Template.documentsIndexItem.helpers({
+	//count number of comments against the problem
+    numberOfComments(problemId) {
+        return Comments.find({problemId:problemId}).count();
+    }
 })
 
 Template.documentsIndexItem.events({
