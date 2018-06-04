78674b238636677a0c3cb2c8c07ba6c2003ed140
Chamidu Supeshala <chamidusupeshala@gmail.com>
2018-06-04 19:04:19 +0530

    problem: redundant login step #130
    solution: allow directly login from topbar, trashed old unused signup templates and routes

--- imports/startup/client/router/router.js
+++ imports/startup/client/router/router.js
@@ -7,7 +7,6 @@ import "./router-config.js"
 import "./filter-config.js"
 
 // Routes
-import "./auth-routes.js"
 import './stats-routes.js'
 import './notifications-routes.js'
 import "./document-routes.js"
--- imports/ui/components/auth/signup/signup.js
+++
@@ -1,9 +0,0 @@
-import { Template } from 'meteor/templating'
-import './signup.html'
-
-Template.signup.events({
-  'submit #signup': function(event) {
-    event.preventDefault();
-    // create new user here
-  }
-})
