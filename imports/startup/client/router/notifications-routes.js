5ed88a9b667569f66723662dfdf3f78e3242f194
Samuel Ralak <samuelralak@hotmail.com>
2018-06-04 17:44:48 +0300

    Problem: CoreUI theme not taken full advantage of #115
    Solution: Changed Emurgis layout. Added a breadcrumb section and sidenav

--- imports/startup/client/router/notifications-routes.js
+++ imports/startup/client/router/notifications-routes.js
@@ -1,9 +1,10 @@
 import { FlowRouter } from 'meteor/kadira:flow-router'
 import { BlazeLayout } from 'meteor/kadira:blaze-layout'
+import { appRoutes } from './group-config'
 
 import '/imports/ui/components/notifications/notifications'
 
-FlowRouter.route('/notifications', {
+appRoutes.route('/notifications', {
     action: () => {
         BlazeLayout.render('layout', {
           main: 'notifications',
