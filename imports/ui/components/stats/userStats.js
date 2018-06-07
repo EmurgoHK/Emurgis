f38f88125d434f391a5d7f3ca5428fd3ad16ad06
Andrej Budinčević <andrew@hotmail.rs>
2018-06-07 22:46:48 +0200

    Problem: No solved problems graph (#205)
    Solution: Show a bar chart with a number of completed problems for each user on the top of the stats page.

--- imports/ui/components/stats/userStats.js
+++ imports/ui/components/stats/userStats.js
@@ -3,12 +3,56 @@ import { FlowRouter } from 'meteor/kadira:flow-router'
 
 import { Stats } from '/imports/api/stats/both/statsCollection.js'
 
+import Chart from 'chart.js'
+
 import './userStats.html'
 
 Template.userStats.onCreated(function() {
     this.autorun(() => {
         this.subscribe('users')
         this.subscribe('userStats')
+    })
+})
+
+Template.userStats.onRendered(function() {
+    const ctx = $('#js-chart')[0].getContext('2d')
+    this.barChart = new Chart(ctx, {
+        type: 'bar',
+        data: {},
+        options: {
+            responsive: true,
+            legend: {
+                display: false,
+                position: 'top',
+            },
+            title: {
+                display: false,
+                text: 'Solved problems chart'
+            }
+        }
+    })
+
+    this.autorun(() => {
+        let users = Meteor.users.find({}).fetch()
+
+        if (users && users.length) {
+            this.barChart.data.labels = users.map(i => (i.profile || {}).name || '')
+
+            this.barChart.data.datasets = [{
+                backgroundColor: '#20a8d8',
+                borderColor: '#fff1f0',
+                borderWidth: 1,
+                data: users.map(i => {
+                    let stats = Stats.findOne({
+                        userId: i._id
+                    })
+
+                    return stats ? (stats.completedProblems || []).length : 0
+                })
+            }]
+
+            this.barChart.update()
+        }
     })
 })
 
