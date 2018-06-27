722693fe8821ff3f1768f6a35a4e66d4abec75e9
gazhayes <gareth.hayes@gmail.com>
2018-06-27 17:03:02 +0900

    problem: auth system is broken (regression)
    solution: DO NOT PURGE OAUTH CREDENTIALS!

--- imports/startup/server/accounts.js
+++
@@ -1,9 +0,0 @@
-// Accounts
-ServiceConfiguration.configurations.remove({
-  service: 'google'
-})
-ServiceConfiguration.configurations.insert({
-  service: 'google',
-  clientId: Meteor.settings.google ? Meteor.settings.google.clientId : '',
-  secret: Meteor.settings.google ? Meteor.settings.google.secret : ''
-})
