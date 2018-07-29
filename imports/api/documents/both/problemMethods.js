c375e5c621b0260eadeeea41838b200594045e3a
Dilpreet Singh <singhsince94@gmail.com>
2018-07-29 13:18:51 +0530

    problem: problems that are ready for review are marked as stale #406
    solution: if a problem is ready for review, never mark it as stale.

--- imports/api/documents/both/problemMethods.js
+++ imports/api/documents/both/problemMethods.js
@@ -730,7 +730,7 @@ export const reopenProblem = new ValidatedMethod({
         }
 
         Problems.update({
-            _id: problemId 
+            _id: problemId
         }, {
             $set: {
                 status: 'open',
@@ -866,7 +866,7 @@ export const checkForStaleProblems = new ValidatedMethod({
     validate: new SimpleSchema({}).validator(),
     run ({}) {
         Problems.find({}).fetch().filter(i => {
-            return (i.status !== 'stale' && i.lastActionTime) ? ((new Date().getTime() - i.lastActionTime) > (10 * 24 * 60 * 60 * 1000)) : false
+            return (i.status !== 'stale' && i.status !== 'closed' && i.status !== 'ready for review' && i.lastActionTime) ? ((new Date().getTime() - i.lastActionTime) > (10 * 24 * 60 * 60 * 1000)) : false
         }).forEach(i => {
             Problems.update({
                 _id: i._id
